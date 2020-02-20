b1da85b9f534a311858e8f71dd0e6584912fc0be
Hushunkang <2752962035@qq.com>
2020-02-21 01:14:49 +0800

    其它

---
+++ others/src/javacore/senior/practice/HelloWorld.java
@@ -0,0 +1,13 @@
+package javacore.senior.practice;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年02月21日 01时11分56秒
+ */
+public class HelloWorld {
+
+
+
+}
