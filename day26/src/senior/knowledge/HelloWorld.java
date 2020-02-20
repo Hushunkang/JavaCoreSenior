b650c5c6558e5b915e17816aa6d143c29e068cf2
Hushunkang <2752962035@qq.com>
2020-02-21 00:28:43 +0800

    day26_IO流

---
+++ day26/src/senior/knowledge/HelloWorld.java
@@ -0,0 +1,15 @@
+package senior.knowledge;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年02月21日 00时04分29秒
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("day26_IO流");
+    }
+
+}
